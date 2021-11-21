4447432168c3ee1eeb26e56fcf4344dc6597d4e1
tanoscar111 <tanoscar2810@gmail.com>
2021-11-21 22:37:37 +0700

    rêact

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -4,8 +4,8 @@ import { addons } from '@storybook/addons';
 addons.setConfig({
   theme: {
     ...themes.dark,
-    brandImage: 'https://hamishw.com/icon.svg',
+    brandImage: '',
     brandTitle: 'Thanh Tan Components',
-    brandUrl: 'https://hamishw.com',
+    brandUrl: '',
   },
 });
